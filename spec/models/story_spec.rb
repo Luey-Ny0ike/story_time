034e5b70b143e6239765adc18233bbbf07212900
lewis nyoike <nyoikelewis@gmail.com>
2017-08-03 11:28:32 +0300

    this commit tests our Story relationship with comments

--- spec/models/story_spec.rb
+++ spec/models/story_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 describe Story do
+  it { should have_many :comments }
   it { should validate_presence_of :title }
   it { should validate_presence_of :image }
   it { should validate_length_of :intro }
